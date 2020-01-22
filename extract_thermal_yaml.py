64192abf8937bf07c5f0aaa397e93f164b3fc506
asif iqbal Bhatti <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-01-22 20:34:37 +0100

    Add files via upload

---
+++ extract_thermal_yaml.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import yaml
+
+with open("thermal_properties.yaml") as f:
+	data = yaml.load(f, Loader=yaml.FullLoader)
+	
+
+# isinstance checks whether the value is a list and if list
+# we count the number of elements it contains.
+# dict = {key, value} pair
+
+count=0
+for x in data:
+	if isinstance(data[x], list):
+		#print(data[x])
+		count += len(data[x]) 
+print("# of elements detected in the list: {}".format(count) )
+
+
+print ("{:1s} {:14.12s} {:14.12s} {}".format(' ','Temperature', 'Entropy', 'Free_energy') )
+for i in range(count):
+	temperature=data['thermal_properties'][i]['temperature']
+	free_energy=data['thermal_properties'][i]['free_energy']
+	entropy=data['thermal_properties'][i]['entropy']
+	heat_capacity=data['thermal_properties'][i]['heat_capacity']
+	energy=data['thermal_properties'][i]['energy']
+	print ("{:10.3f} {:15.8f}  {:15.8f}".format(temperature, entropy, free_energy) )
